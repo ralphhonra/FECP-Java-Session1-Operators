d5f3d6a443f5e79ad0c240ba07625b87dac2980c
Ralph Daniel Honra <ralphdaniel.honra@GXCHLAP00376.local>
2025-06-30 14:48:47 +0800

    Final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,13 @@ import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Enter temperature in Celsius: ");
+        float inputtedCelsius = new Scanner(System.in).nextFloat();
 
+        System.out.print("Temperature in Fahrenheit: " + String.format("%.2f", getFahrenheit(inputtedCelsius)));
+    }
+
+    public static float getFahrenheit(float inputtedCelsius) {
+        return inputtedCelsius * (9/5.0f) + 32;
     }
 }
